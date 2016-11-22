b28fd834a98c622ed3f47d870fd8d255752a3f0f
Stephanie Frankian class of 2017 <sfrankia@tempest.wellesley.edu>
2016-11-21 19:40:06 -0500

    added home.html template

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/local/bin/python2.7
+
+''' Stephanie Frankian and Elizabeth Kuszmaul
+    November 20, 2016
+    main.py
+'''
+
+import db_functions
+from flask import Flask, request,render_template, redirect, url_for
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    render_template("home.html")
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run('0.0.0.0', 9874)

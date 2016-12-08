276c355591192e480992b3fa223439b439962734
Stephanie Frankian class of 2017 <sfrankia@tempest.wellesley.edu>
2016-12-08 15:03:23 -0500

    fixing the table  structure

--- Poll.sql
+++ Poll.sql
@@ -1,15 +1,5 @@
 -- Stephanie Frankian and Elizabeth Kuszmaul
 
--- Creating the SQL tables for our application
-
--- drop table if exists users;
--- create table users(
---       uid int not null primary key,
---       email varchar(65) not null,
---       pwd varchar(22) not null,
---       INDEX (uid)      
--- ) ENGINE = InnoDB;
-
 drop table if exists poll;
 create table poll(
        id int not null primary key auto_increment,
@@ -21,19 +11,22 @@ create table poll(
 
 drop table if exists poll_options;
 create table poll_options(
-       oid int not null primary key auto_increment,
        poll_id int not null,
+       oid int not null,
        location varchar(30),
        given_time time,
-       INDEX (poll_id)
+       INDEX (oid),
+       INDEX (poll_id),
+       foreign key (poll_id) references poll(id) on delete cascade
 ) ENGINE = InnoDB;
 
 drop table if exists responses;
 create table responses(
        poll_id int not null,
        oid int not null,
+       primary key (poll_id,oid),
        response smallint not null,
        INDEX (poll_id),
        INDEX (oid),
-       foreign key (oid) references poll_options(oid) on delete cascade
+       foreign key (poll_id) references poll(id) on delete cascade
 ) ENGINE = InnoDB;

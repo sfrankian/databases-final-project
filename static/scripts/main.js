5561a3b008c8c0a5fafc1d40d11c1c75c38fd10a
Stephanie Frankian class of 2017 <sfrankia@tempest.wellesley.edu>
2016-12-20 12:01:30 -0500

    fixed create form and added datepicker

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -5,42 +5,42 @@
 // Credit to http://stackoverflow.com/questions/19578111/how-to-add-clone-form-fields-using-jquery-and-increment-ids-and-names
 // For helping us figure out how to clone form fields!
 
-$(document).ready(function(){
-	var time_index = 0;
-	var location_index = 0;
-	// If the time button is clicked
-	$("#add_time").click(function(){
-		time_index++;
-		// Cloning and making the new field visible
-		$(this).parent().before($("#time_form").clone().attr("id","time_form" + time_index));
-		$("#time_form" + time_index).css("display","inline");
-		$("#time_form" + time_index + " :input").each(function(){
-			// Making the name and id use the clone's number	
-			$(this).attr("name",$(this).attr("name") + time_index);
-			$(this).attr("id",$(this).attr("id") + time_index);
-		    });
-		// Removing a time field
-		$("#remove_time" + time_index).click(function(){
-			$(this).closest("div").remove();
-		    });
-	}); 
-    
-	// If the location button is clicked
-	$("#add_location").click(function(){
-		location_index++;
-		// Cloning and making the new field visible
-		$(this).parent().before($("#location_form").clone().attr("id","location_form" + location_index));
-		$("#location_form" + location_index).css("display","inline");
-		$("#location_form" + location_index + " :input").each(function(){
-			// Making the name and id use the clone's number	
-			$(this).attr("name",$(this).attr("name") + location_index);
-			$(this).attr("id",$(this).attr("id") + location_index);
-		    });
-		// Removing a location field
-		$("#remove_location" + location_index).click(function(){
-			$(this).closest("div").remove();
-		    });
-	}); 
-    
-       
- });
+$(document).ready(function() {
+	var row_index=0;
+	var table = document.getElementById("form_table");
+	$("#add_row").click(function(){
+		row_index++;
+		var input = document.createElement("input");
+		input.type = "text";
+		
+		var row = table.insertRow(-1);
+		var col0 = row.insertCell(0);
+		var col1 = row.insertCell(1);
+		var col2 = row.insertCell(2);
+		
+	
+		var newDate = document.createElement("input");
+		newDate.type = "input";
+		newDate.id = "date" + row_index;
+		newDate.className = "pickDate";
+		$(newDate).appendTo(col0);
+		$(newDate).datepicker();
+			
+		
+		input.name = "time" + row_index;
+		col1.appendChild(input);
+		
+		$(input).clone().prop('name','location'+row_index).appendTo(col2);
+		
+	
+	});
+	
+	$("#remove_row").click(function(){
+		if(row_index > 0) {
+			document.getElementById("form_table").deleteRow(-1);
+			row_index--;
+		}
+		
+	
+	});
+});
